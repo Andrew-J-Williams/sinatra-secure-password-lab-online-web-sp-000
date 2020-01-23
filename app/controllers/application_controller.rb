31dade61b70986d2c0ffe585e9bab0a9216b4e62
Andrew Williams <1williamsaj@gmail.com>
2020-01-23 04:28:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,8 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect '/account'
     else
-      redirect '/failure' 
+      redirect '/failure'
+    end 
 
   end
 
